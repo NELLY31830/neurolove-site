1771b4fd929a3354d90c2f86ef095b948f57b959
NELLY31830 <nelly.paredes31@yahoo.com>
2025-03-25 14:32:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Application from './App';
-import './style.css';
+import'./style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Application />
-  </React.StrictMode>
+  <Application/>
+  </ReactStrictMode>
 );
