8ec9b32ef3133265761dc1e459926352a1a6d2a2
NELLY31830 <nelly.paredes31@yahoo.com>
2025-03-25 13:20:29 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Application() {
+  return (
+    <div className="Application">
+      <h1>Bienvenue sur NeuroLove</h1>
+      <p>Une application de rencontres basée sur la compatibilité 🧠💫</p>
+    </div>
+  );
+}
+
+export default Application;
